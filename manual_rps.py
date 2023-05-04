9c9eeb985cd6b9dcb7291e5af83bc56b9e3993c8
arif9011 <115591569+arif9011@users.noreply.github.com>
2023-05-04 21:54:04 +0100

    Add files via upload

--- manual_rps.py
+++ manual_rps.py
@@ -1,8 +1,10 @@
 import random
-class rock_paper_scissors:
+class rps:
       
     def __init__(self, computer_list):
-        self.computer_list = computer_list  
+        self.computer_list = computer_list
+        self.computer_wins = 0
+        self.user_wins = 0
         pass  
     def get_computer_choice(self):
         computer_choice = random.choice(self.computer_list)
@@ -12,27 +14,34 @@ class rock_paper_scissors:
         user_choice = input("Enter your choice and play rock paper scissors game:")
         return user_choice
               
-    def get_winner(self, computer_choice, user_choice):
+    def get_winner(self,computer_choice,user_choice):
+        result=0
+        print(f"Computer Chooses: {computer_choice} and user chooses: {user_choice}")
+        if user_choice == "nothing":
+            print("Choose something")
+    
+        elif computer_choice == user_choice:
+            print("It's a tie!")
+            result = "tie"
+            # Combinations for user to win
+        elif (computer_choice == "rock" and user_choice == "paper") or (computer_choice == "paper" and user_choice == "scissors") or (computer_choice == "scissors" and user_choice == "rock"):
+            
+            print("User win")
+            result = "User"
+    # All other Combinations means computer wins
+        else:
+            print("Computer win")
+            result = "Computer"
+        return result      
+def play():  
+        computer_list = ['rock','paper','scissors']
+        game=rps(computer_list)
+        computer_choice = game.get_computer_choice()
+        print(computer_choice)
+        user_choice = game.get_user_choice()
+        game.get_winner(computer_choice, user_choice)
         
-        print(f"The computer picked {computer_choice}, you picked {user_choice}")
-
-        if computer_choice == "paper" and user_choice == "rock" or computer_choice == "scissors" and user_choice == "paper" or computer_choice == "rock" and user_choice == "scissors":
-            print ("You won!") 
-            pass
-        elif computer_choice == "rock" and user_choice == "paper" or computer_choice == "paper" and user_choice == "scissors" or computer_choice == "scissors" and user_choice == "rock":
-            print ("You lost")
-            pass
-        else: 
         
-            print("It is tie")
-
-def play(computer_list = ['rock','paper','scissors']):
-    rps = rock_paper_scissors(computer_list)
-    computer_choice = rps.get_computer_choice()
-    print(computer_choice)
-    user_choice = rps.get_user_choice()
-    rps.get_winner(computer_choice, user_choice)
-
 
 play()
 
